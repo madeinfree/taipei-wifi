e75fe840ac46fb4647103f88df52aef58eaf2721
whien <sal95610@gmail.com>
2016-04-16 01:13:41 +0800

    added hot module

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,17 +1,25 @@
-var path = require("path");
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
   entry: {
-    app: ["./app/main.react.js"]
+    app: [ './app/main.react.js' ],
+    vendors: [ 'webpack-dev-server/client?http://localhost:8080', 'webpack/hot/only-dev-server' ]
   },
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
   },
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'app')
+      exclude: /node_modules/,
+      loaders: ['react-hot', 'babel']
     }]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 };
