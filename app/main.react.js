491ff07f0f4299ce98bf795f9b2ad62b2e5cdf7a
whien <sal95610@gmail.com>
2016-04-16 01:13:58 +0800

    basic main component

--- app/main.react.js
+++ app/main.react.js
@@ -1,1 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDom, { render } from 'react-dom';
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Hello Taipei Wifi.</div>
+    );
+  }
+};
+
+render(<Main />, document.getElementById('app'));
