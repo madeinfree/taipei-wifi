648cee3a8281df1b208e93c17c9d31ff30630a71
whien <sal95610@gmail.com>
2016-04-16 12:15:52 +0800

    remove header from main

--- app/main.react.js
+++ app/main.react.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import App from './components/common/app.react';
-import Header from './components/common/header.react';
 
 function Main() {
   return (
     <div>
-      <Header />
       <App />
     </div>
   );
