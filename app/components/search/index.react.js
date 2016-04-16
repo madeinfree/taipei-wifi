f16fe78a298085eeda65e0a1eb24a8b00cef395a
whien <sal95610@gmail.com>
2016-04-16 12:17:19 +0800

    added search and about component

---
+++ app/components/search/index.react.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Search extends React.Component {
+  static defaultProps = {}
+
+  render() {
+    return (
+      <div style={ { padding: '30px' } }>
+        Search...
+      </div>
+    );
+  }
+}
+
+export default Search;
